75bc544d65bba703148fb6e25b371a7a88bfe6b2
Richard Ross <richard.ross@sci-telligent.co.uk>
2021-03-12 09:40:41 +0000

    initial routes defined, User model added

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/sponsor-profiles', require('./routes/api/sponsor-profiles'));
+app.use('/api/pitcher-profiles', require('./routes/api/pitcher-profiles'));
+app.use('/api/projects', require('./routes/api/projects'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
