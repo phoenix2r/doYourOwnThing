e86fcef8406a72bba0dfad8786d94bb1f4c641d9
Richard Ross <richard.ross@sci-telligent.co.uk>
2021-03-12 16:55:59 +0000

    JWT added

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // connect database
 connectDB();
 
+// Init Middleware - to parse body requests
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes
