66f4d4e35551180c96d8d754820a1b3594a3e5bb
Richard Ross <richard.ross@sci-telligent.co.uk>
2021-04-19 11:18:50 +0100

    modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,7 +10,7 @@ connectDB();
 // Init Middleware - to parse body requests
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -17,6 +18,16 @@ app.use('/api/sponsor-profiles', require('./routes/api/sponsor-profiles'));
 app.use('/api/pitcher-profiles', require('./routes/api/pitcher-profiles'));
 app.use('/api/projects', require('./routes/api/projects'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
