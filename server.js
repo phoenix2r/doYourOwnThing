9a24aed19a7d15a34df71efb2dc63e5d9f64a825
Richard Ross <richard.ross@sci-telligent.co.uk>
2021-04-19 15:51:44 +0100

    resolved server conflict

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 // Init Middleware - to parse body requests
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
